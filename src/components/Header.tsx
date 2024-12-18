af78e3b436b24b800bb57f834b22b723ba886d9d
nicjrk <116735056+nicjrk@users.noreply.github.com>
2024-12-18 18:27:51 +0200

    Schimbare nr

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,9 +30,11 @@ const Header = () => {
       "Doriți să sunați la SMART ROOF CONSTANTIN?\nNumăr: +40 (742) 691 135"
     );
     if (confirmCall) {
+      // Inițiază apelul direct
       window.location.href = "tel:+40742691135";
     }
   };
+  
 
   return (
     <header
